5e6c202084bc65688c5f7246f9495b326c3c15ec
Costigan-Stephen <stephenjcostigan@hotmail.com>
2021-09-26 00:15:51 -0500

    push stuff

---
+++ cms/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { HeaderComponent } from './header.component';
+import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactsComponent,
+    HeaderComponent,
+    ContactDetailComponent,
+    ContactListComponent,
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
