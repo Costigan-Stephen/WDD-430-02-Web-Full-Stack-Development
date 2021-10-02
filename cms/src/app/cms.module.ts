064da3192e74293eb32b5d2c906e69b60a18c127
Costigan-Stephen <stephenjcostigan@hotmail.com>
2021-10-01 23:55:21 -0500

    completed w3

--- cms/src/app/cms.module.ts
+++ cms/src/app/cms.module.ts
@@ -9,7 +9,16 @@ import { ContactsComponent } from './contacts/contacts.component';
 import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 import { ContactItemComponent } from './contacts/contact-item/contact-item.component';
-
+// DOCUMENTS
+import { DocumentsComponent } from './documents/documents.component';
+import { DocumentListComponent } from './documents/document-list/document-list.component';
+import { DocumentDetailComponent } from './documents/document-detail/document-detail.component';
+import { DocumentItemComponent } from './documents/document-item/document-item.component';
+// MESSAGES
+import { MessagesComponent } from './messages/messages.component';
+import { MessagesItemComponent } from './messages/messages-item/messages-item.component';
+import { MessagesListComponent } from './messages/messages-list/messages-list.component';
+import { MessagesEditComponent } from './messages/messages-edit/messages-edit.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +28,14 @@ import { ContactItemComponent } from './contacts/contact-item/contact-item.compo
     ContactDetailComponent,
     ContactListComponent,
     ContactItemComponent,
+    MessagesComponent,
+    DocumentsComponent,
+    DocumentListComponent,
+    DocumentDetailComponent,
+    DocumentItemComponent,
+    MessagesItemComponent,
+    MessagesListComponent,
+    MessagesEditComponent,
   ],
   imports: [
     BrowserModule
